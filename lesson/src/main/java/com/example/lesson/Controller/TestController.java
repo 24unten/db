08cb7c2902b38ac01faf7ec44d80c624951b3f0b
unten <24unten_miku@axiz.co.jp>
2024-05-24 16:10:55 +0900

    test

--- lesson/src/main/java/com/example/lesson/Controller/TestController.java
+++ lesson/src/main/java/com/example/lesson/Controller/TestController.java
@@ -3,6 +3,8 @@ package com.example.lesson.Controller;
 import com.example.lesson.Product.Product;
 import com.example.lesson.Service.ProductService;
 import com.example.lesson.form.AddForm;
+import com.example.lesson.form.UpdateForm;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,16 +28,13 @@ public class TestController {
         return "product-list";
     }
 
+
     @GetMapping("/product/{id}")
     public String product(@PathVariable("id") int id ,Model model) {
         model.addAttribute("products",productService.findById(id));
         return "product";
     }
 
-    @GetMapping("/product-add")
-    public String index(@ModelAttribute("addForm" ) AddForm addForm){
-        return "product-add";
-    }
 
     @PostMapping("/product-add")
     public String add(@Validated @ModelAttribute("addForm" ) AddForm addForm, BindingResult bindingResult) {
@@ -47,6 +46,37 @@ public class TestController {
         }
 
     }
+
+    @GetMapping("/product-add")
+    public String index(@ModelAttribute("addForm" ) AddForm addForm){
+        return "product-add";
+    }
+
+
+    @GetMapping("/update/{id}")
+    public String update2(@ModelAttribute("updateForm") UpdateForm updateForm,@PathVariable("id") int id ,Model model) {
+        model.addAttribute("products",productService.findById(id));
+        return "update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String update2(@PathVariable("id") int id ,@Validated @ModelAttribute("updateForm") UpdateForm updateForm, BindingResult bindingResult,Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("products",productService.findById(id));
+            return "/update";
+        } else {
+            productService.update(new Product(id, updateForm.getName(),updateForm.getPrice()));
+            return  "redirect:/product-list";
+        }
+
+    }
+
+    @PostMapping("/product-delete/{id}")
+    public String delete(@PathVariable("id") int id ,Model model) {
+        productService.delete(id);
+        return "redirect:/product-list";
+    }
+
 
 
 
